fa6752ff0a3665c8ce67dfd0f1dcd3a83a7d365c
Prakhar Gupta <prakhar.gupta.cse14@itbhu.ac.in>
2015-03-30 09:14:33 -0700

    Update ClassInterpolate.py

--- ClassInterpolate.py
+++ ClassInterpolate.py
@@ -1,47 +1,41 @@
-Class Interpolate:
- 
-    def solve(self,L,M,method):
-        if(method=="newton"):
-            return (self.Newton(L,M))
-        else:
-            return (self.Lagrange(L,M))
-
-    def Lagrange(self,L,M):                                                
-       
-        
-        from numpy import array
-        from numpy.polynomial import polynomial as P
-        n=len(L)                                                           
-        x=(-1*L[0],1)                                                      
-        for i in range(1,n):
-            x=P.polymul(x,(-1*L[i],1))                                    
-        result=array([0.0 for i in range(len(x)-1)])                    
-        derivative=P.polyder(x)                                             
+##ques 4
+from numpy import *
+import matplotlib.pyplot as plt
+class interpolate:
+ def __init__(self,lx,fx):
+     self.lx=lx
+     self.fx=fx
+ def lagrange(self):
+     wx=poly1d(poly(self.lx))
+     wxp=polyder(wx)
+     list=[wxp(i) for i in self.lx]
+     goa=poly1d([1])
+     for j in range(0,len(self.lx)):
+         a=self.lx[j]
+         self.lx.remove(self.lx[j])
+         goa=polyadd(goa,(poly1d(poly(self.lx)))*(self.fx[j]/list[j]))
+         self.lx.insert(j,a)
+     goa=goa-1
+     return(goa)
+ def Newton(self):
+        n=len(self.lx)
+        mat=[[0.0 for i in range(n)] for j in range(n)]
         for i in range(n):
-            result+=(P.polydiv(x,(-1*L[i],1))[0]*M[i])/P.polyval(L[i],derivative)   
-        return(list(result))   
-    def Newton(self,L,M):                                                   
-       
-        
-        from numpy import array
-        from numpy.polynomial import polynomial as P
-        n=len(L)                                                            
-        mat=[[0.0 for i in range(n)] for j in range(n)]                    
-        for i in range(n):                                                 
-            mat[i][0]=M[i]
-        for i in range(1,n):                                               
-            for j in range(n-i):
-                mat[j][i]=(mat[j+1][i-1]-mat[j][i-1])/(L[j+i]-L[j])
-        result=array((mat[0][0],))                                          
+            mat[i][0]=self.fx[i]
         for i in range(1,n):
-            prod=(-1*L[0],1)                                               
-                                                                            
-            for j in range(1,i):
-                prod=P.polymul(prod,(-1*L[j],1))                              
-            result=P.polyadd(result,array(prod)*mat[0][i])                  
-        return (list(result))                                               
+            for j in range(n-i):
+                mat[j][i]=(mat[j+1][i-1]-mat[j][i-1])/(self.lx[j+i]-self.fx[j])
+        result=array((mat[0][0],))
+        for i in range(1,n):
+            prod=(-1*self.lx[0],1)
 
-apx=Interpolate()                                                          
-for method in ["newton","lagrange"]:
-    solution=apx.solve([1,2,3],[0,-1,0],method)
-    print(solution)
+            for j in range(1,i):
+                prod=polymul(prod,(-1*self.lx[j],1))
+            result=polyadd(result,array(prod)*mat[0][i])
+        return (list(result))
+ def graph(self,a,b):
+     x=arange(a,b,0.01)
+     y=self.lagrange()
+     plt.plot(x,y(x),'b-')
+     plt.plot(self.lx,self.fx,'ro')
+     plt.show()
